756cf6167a1aa97e377180357608808fb56ed53d
Steven Garcia <steven.garca.97@gmail.com>
2018-11-30 10:40:00 -0500

    V0.0002

---
+++ src/main/java/com/crunchify/jsp/servlet/ServletAdministrador.java
@@ -0,0 +1,75 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crunchify.jsp.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.co.sergio.mundo.dao.AministradorDAO;
+import edu.co.sergio.mundo.vo.Administrador;
+import java.io.IOException;
+
+/**
+ *
+ * @author fabian.giraldo
+ */
+public class ServletAdministrador extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        AministradorDAO daoAdmin = new AministradorDAO();
+        List<Administrador> Admin = daoAdmin.findAll();
+        //request.setAttribute("", escuelas);
+        //Redireccionando la informacion
+        RequestDispatcher redireccion = request.getRequestDispatcher("index.jsp");
+        redireccion.forward(request, response);
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+// </editor-fold>
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -18,64 +18,7 @@ body {
     </head>
     <body>
         <div align="center" style="margin-top: 50px;">
-            <%
-              if(request.getAttribute("escuelas")!=null){
-            %>   
-            <form action="Escuela" method="post">
-                <select name = "escuela">
-           <%
-              List<Escuela> escuelas  = (List<Escuela>)request.getAttribute("escuelas");  
-               for (Escuela escuela : escuelas) {
-             %>
-               <option value="<%=escuela.getCodigo()%>"><%=escuela.getNombre()%></option>
-             
-             <%
-               }
-           %>
-                </select>
-                <input type="submit" value="submit">
-            </form>
-            <%
-              }
-            
-            %>
-            
-          
-        </div>
-
-
-        <div align="center" style="margin-top: 50px;">
-            
-            <%
-              if(request.getAttribute("estudiantes")!=null){
-            %>      
-              <table style="width:100%">
-               <tr>
-                    <th>Codigo</th>
-                    <th>Nombre</th> 
-                    <th>Escuela</th>
-              </tr>
-                
-              <%
-              List<Estudiante> estudiantes  = (List<Estudiante>)request.getAttribute("estudiantes");  
-               for (Estudiante estudiante : estudiantes) {
-             %>
-             
-               <tr>
-                  <td><%=estudiante.getCodigo()%></td>
-                  <td><%=estudiante.getNombre()%></td> 
-                  <td><%=estudiante.getEscuela()%></td>
-              </tr>
-             
-             <%
-             }
-             %>
-               
-                </table>
-               
-            <%
-              }
-            %>   
+            <h1>Hola!!!</h1>   
             
         </div>
     
